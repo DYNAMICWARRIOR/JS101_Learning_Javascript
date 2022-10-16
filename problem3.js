9bf01256dd882b613b67ba21c3842f56b9058cf3
Subhransu Pradhan <64146297+DYNAMICWARRIOR@users.noreply.github.com>
2022-10-16 23:20:35 +0530

    Add files via upload

---
+++ problem3.js
@@ -0,0 +1,9 @@
+//Problem 3
+//Create a variable name to store your name
+//Create a variable age to store your age
+//Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name = "Subhransu";
+let age = 28;
+console.log(name);
+console.log(age);
---
+++ problem4.js
@@ -0,0 +1,22 @@
+//Problem 4
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+//Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+let name = "Subhransu";
+let school = "Masai";
+let grade = "web23";
+let section = "A";
+let roll_no = 0453;
+let html = 90;
+let css = 80;
+let JavaScript = 70;
+
+console.log("Name :",name);
+console.log("School :",school);
+console.log("Grade :",grade);
+console.log("Section :",section);
+console.log("Roll No.:",roll_no);
+console.log("HTML:",html);
+console.log("CSS :",css);
+console.log("JavaScript :",JavaScript);
