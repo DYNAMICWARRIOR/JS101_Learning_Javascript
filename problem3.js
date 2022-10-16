a9969cbd9cc7e04de647c1dbc36e57c5e7502feb
Subhransu Pradhan <64146297+DYNAMICWARRIOR@users.noreply.github.com>
2022-10-16 23:24:23 +0530

    Delete problem3.js

--- problem3.js
+++
@@ -1,9 +0,0 @@
-//Problem 3
-//Create a variable name to store your name
-//Create a variable age to store your age
-//Print the values stored in the variable on one line followed by the type of the variable in the next line
-
-let name = "Subhransu";
-let age = 28;
-console.log(name);
-console.log(age);
